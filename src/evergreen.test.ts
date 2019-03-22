cb772369d19d42bb57d0fd19738891083a6bee42
John Liu <john.liu@mongodb.com>
2019-03-22 15:26:19 -0400

    add some requests, tests

--- src/evergreen.test.ts
+++ src/evergreen.test.ts
@@ -1,12 +1,80 @@
-import { Evergreen } from "./evergreen";
+import * as fs from "fs";
+import * as yaml from "js-yaml";
+import * as os from "os";
+import * as request from "request";
+import { evergreen } from './evergreen';
+
+const configFilePath = os.homedir() + "/.evergreen.yml";
+
+function getEvergreenClient(): evergreen.client {
+    let user: string;
+    let key: string;
+    let serverURL: string;
+    if (fs.existsSync(configFilePath)) {
+        const data = fs.readFileSync(configFilePath, "utf8");
+        const localConfig = yaml.safeLoad(data);
+        user = localConfig.user;
+        key = localConfig.api_key;
+        serverURL = localConfig.api_server_host;
+    } else {
+        user = process.env.API_USER || "";
+        key = process.env.API_KEY || "";
+        serverURL = process.env.API_SERVER || "";
+    }
+
+    return new evergreen.client(user, key, serverURL);
+}
 
 test("Evergreen client is constructed correctly", () => {
     const user = "me";
-    const key= "abc123";
+    const key = "abc123";
     const serverURL = "www.example.com";
-    let evergreen = new Evergreen(user, key, serverURL);
+    const client = new evergreen.client(user,key,serverURL)
 
-    expect(evergreen.Username).toBe(user);
-    expect(evergreen.Key).toBe(key);
-    expect(evergreen.ServerURL).toBe(serverURL);
+    expect(client.username).toBe(user);
+    expect(client.key).toBe(key);
+    expect(client.serverURL).toBe(serverURL);
+});
+
+test("query strings are formed correctly", () => {
+    let params1 = {
+        "param1": "val1",
+        "param2": true,
+        "param3": 55,
+    }
+    expect(evergreen.queryString(params1)).toBe("?param1=val1&param2=true&param3=55");
+
+    let params2 = {
+        "undefined": undefined,
+        "null": null,
+    }
+    expect(evergreen.queryString(params2)).toBe("");
+
+    expect(evergreen.queryString({})).toBe("");
+})
+
+test("integration tests with real API, credentials", (done) => {
+    const client = getEvergreenClient();
+    const toTest: {(callback: request.RequestCallback): void;}[] = [
+        client.getDistros,
+        client.getRecentTasks,
+    ];
+    
+    let count = 0;
+    const callback = (error: any, response: request.Response, body: any): void => {
+        expect(error).toBe(null);
+        expect(response.statusCode).toBe(200);
+        expect(body).not.toBe(null);
+        expect(body).not.toBe(undefined);
+        if (count < toTest.length - 1) {
+            count++;
+        } else {
+            done();
+        }
+    };
+
+    toTest.forEach((fn) => {
+        console.log("testing " + fn.name);
+        client[fn.name](callback);
+    })
 })
